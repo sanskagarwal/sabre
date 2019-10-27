b03d625b2a9352f64e91b86e7d2cd18f32ab48b5
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2019-10-27 11:58:23 +0530

    Styled pages

--- routes/index.js
+++ routes/index.js
@@ -47,9 +47,9 @@ router.post("/login", passport.authenticate("local", {
   res.redirect('/dashboard');
 });
 
-router.post("/logout", function (req, res) {
+router.get("/logout", function (req, res) {
   req.logout();
   res.redirect('/');
-})
+});
 
 module.exports = router;
